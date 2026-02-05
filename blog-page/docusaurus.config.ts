6ec38606a795ab609c46ef9317ecb9045a384bc5
afnad ca <afnadca2@gmail.com>
2026-02-05 12:04:51 +0530

    refactor: update edit url

--- blog-page/docusaurus.config.ts
+++ blog-page/docusaurus.config.ts
@@ -57,9 +57,8 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/vicharanashala/vled/tree/main/',
 
-            // https://github.com/vicharanashala/vled.git
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
